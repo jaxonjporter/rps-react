75eb525a43e08ec82e4eac4f7a14cb43d3e147b9
Jaxon Porter <jaxonutah@gmail.com>
2019-03-14 16:13:42 -0600

    added player icons, styling, and playerOne class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import {Image, Col, Row, Container,  } from "react-bootstrap";
+import twoPlayer from "./images/twoplayers.svg"
+import onePlayer from "./images/oneplayer.svg"
+import OnePlayer from "./OnePlayer.js"
+import {Image, Col, Row, Container, Button,  } from "react-bootstrap";
 
 class App extends Component {
+  state = {
+    onePlayer: true,
+    twoPlayer: "false",
+    game: false,
+    rps: { rock: false, paper: false, scissors: false,}
+  }
+
+  toggleOnePlayer = () => this.setState({onePlayer: !this.state.onePlayer})
+
+  toggletwoPlayer = () => this.setState({twoPlayer: !this.state.twoPlayer})
+
+  
+
   render() {
     return (
-      <Container className="bg-dark text-light text-center">
+      <Container className="bg-dark text-light text-center mt-5 shadow-lg rounded" style={{minHeight: "450px"}}>
         <Row>
-          <Col>
-          <h1>Rock</h1>
+          <Col xs={12}>
+            <h1>Welcome To Rock Paper scissors</h1>
           </Col>
-          <Col>
-          <h1>Paper</h1>
-          </Col>
-          <Col>
-          <h1>Scissors</h1>
-          </Col>
+          {this.state.onePlayer ? 
+          <Row>
+            <Col xs={6}>
+              <Button variant="outline-primary" onClick={this.toggleOnePlayer} style={{height: "300px", width: "100%", marginLeft: "30%"}}><Image src={onePlayer} style={{ height: "280px"}} /></Button>
+            </Col>
+            <Col xs={6}>
+              <Button variant="outline-warning" style={{ height: "300px", width: "100%", marginLeft: "50%"}}><Image src={twoPlayer} style={{ width:"100%"}} /></Button>
+            </Col> 
+          </Row> : <OnePlayer />}
         </Row>
       </Container>
     );
---
+++ src/OnePlayer.js
@@ -0,0 +1,17 @@
+import React, {Component, } from "react";
+
+class OnePlayer extends Component {
+
+  render () {
+    return (
+      <div className="col-12">
+        <h3 style={{color: "#a3a3a3"}}>Pick Your Weapon</h3>
+      
+      </div>
+    )
+  }
+}
+
+
+
+export default OnePlayer
